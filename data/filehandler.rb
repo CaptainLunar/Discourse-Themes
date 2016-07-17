4e48acd173e39020942c8b8d886b60a520c3ef7e
lunar lunar lunar <captainlunar@gmail.com>
2016-07-17 12:44:58 -0500

    help

---
+++ data/filehandler.rb
@@ -0,0 +1,17 @@
+# name: DiscourseThemes -> File Handler
+# about: Handles the saving of css files
+# version: 0.0.1
+# authors: lunarmuffins
+
+#Ideas
+#This will allow the user to save CSS files on the server side.
+#After we have saved a CSS file on the server side, the file will be available for all clients including admins/users
+#Then, if a user picks a css theme we can save it in their browser using HTML5 localStorage
+
+
+#TODO
+#Create File handling class. Save everything admin uploads in CSS-Files.
+#Only the admin can add CSS files to the plugin.
+#Users can only see the files and now directly modify in any ways
+#Users can only get a copy saved in their localStorage
+#If user changes themes, it will override their settings in localStorage

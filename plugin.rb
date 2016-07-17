f4fcf32e4ed14ce1beb34f6225a76bbebbb73d43
lunar lunar lunar <captainlunar@gmail.com>
2016-07-17 00:21:58 -0500

    test

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,5 @@
 # about: Make Discourse have users be able to switch from available forum themes on the fly. 
 # version: 0.0.1
 # authors: lunarmuffins
+
+enabled_site_setting: :Discourse_Themes_enabled
